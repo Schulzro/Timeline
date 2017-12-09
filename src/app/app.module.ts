0cc2f76770587a891875c02bda15da3c529e6f8e
Schulz <benschuro@gmail.com>
2017-12-09 19:53:14 +0100

    add material and timeline component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { TimelineItemComponent } from './timeline-item/timeline-item.component';
+import { TimelineMaterialModule } from './shared/timeline-material.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TimelineMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/timeline-material.module.ts
@@ -0,0 +1,11 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [],
+    imports: [
+      BrowserAnimationsModule
+    ]
+  })
+  export class TimelineMaterialModule { }
